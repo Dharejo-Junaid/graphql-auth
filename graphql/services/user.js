72ff30110d90ff56ab6b994262732aa7493b6dda
Dharejo-Junaid <junaidali.100190@gmail.com>
2023-10-13 17:57:59 +0000

    setup login

--- graphql/services/user.js
+++ graphql/services/user.js
@@ -3,54 +3,62 @@ const bcrypt = require("bcrypt");
 const { generateToken } = require("../../helpers/jwt");
 
 const typeDefs = `#graphql
-    type User {
-        username: String!,
-        email: String!
-        verified: Boolean!
-    },
 
-    input SignupInput {
-        username: String!,
-        email: String!,
-        password: String!
-    },
+  input SignupInput {
+    username: String!,
+    email: String!,
+    password: String!
+  },
 
-    input LoginInput {
-        email: String!
-        password: String!
-    }
+  input LoginInput {
+    email: String!
+    password: String!
+  }
 
-    type Query {
-        login(user: LoginInput): String!
-    }
+  type Query {
+    login(user: LoginInput): String!
+  }
 
-    type Mutation {
-        signup(user: SignupInput!): String!,
-    }
+  type Mutation {
+    signup(user: SignupInput!): String!,
+  }
 `;
 
 const resolvers = {
   Query: {
-    login: async () => {},
+    login: async (_, { user }) => {
+      const { email, password } = user;
+
+      user = await User.findOne({ email });
+      if (!user) throw new Error("User does not exist");
+
+      if (!user.verified) throw new Error("Account is not verified");
+
+      if (!(await bcrypt.compare(password, user.password)))
+        throw new Error("Incorrect password");
+
+      const token = generateToken(user._id);
+      return token;
+    },
   },
   Mutation: {
     signup: async (_, { user }) => {
       const { username, email, password } = user;
 
-      const existingUser = await User.findOne({ email: email.toLowerCase() });
-      if (existingUser) throw new Error("This email already exists");
+      user = await User.findOne({ email: email.toLowerCase() });
+      if (user) throw new Error("This email already exists");
 
       const emailExp = RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
       if (!emailExp.test(email)) throw new Error("Not a valid email");
 
       const hashPass = await bcrypt.hash(password, 10);
-      const newUser = await User.create({
+      user = await User.create({
         username,
         email: email.toLowerCase(),
         password: hashPass,
       });
 
-      const token = generateToken(newUser._id);
+      const token = generateToken(user._id);
       return token;
     },
   },
